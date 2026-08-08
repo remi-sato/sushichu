fce98e7d670d508bfd7ea342b8656d9fcc60f025
Ubuntu <ubuntu@ip-172-31-36-222.ap-northeast-1.compute.internal>
2026-08-08 07:22:03 +0000

    Add sushi seed data with images

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,54 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+maguro = Sushi.create!(
+  name: "まぐろ", 
+  price: 150, 
+  description: "三崎産の美味しいまぐろ"
+)
+
+maguro.image.attach(
+  io: File.open(Rails.root.join("db/seed_images/maguro.png")),
+  filename: "maguro.png"
+)
+
+salmon = Sushi.create!(
+  name: "サーモン", 
+  price: 150, 
+  description: "定番のおいしさ"
+)
+
+salmon.image.attach(
+  io: File.open(Rails.root.join("db/seed_images/salmon.png")),
+  filename: "salmon.png"
+)
+
+squid = Sushi.create!(
+  name: "いか", 
+  price: 150, 
+  description: "いかと大葉の握り"
+)
+
+squid.image.attach(
+  io: File.open(Rails.root.join("db/seed_images/squid.png")),
+  filename: "squid.png"
+)
+
+egg = Sushi.create!(
+  name: "たまご", 
+  price: 100, 
+  description: "子供も大好き" 
+)
+
+egg.image.attach(
+  io: File.open(Rails.root.join("db/seed_images/egg.png")),
+  filename: "egg.png"
+)
+
+shrimp = Sushi.create!(
+  name: "えび", 
+  price: 200, 
+  description: "アルゼンチン産赤エビ"
+)
+
+shrimp.image.attach(
+  io: File.open(Rails.root.join("db/seed_images/shrimp.png")),
+  filename: "shrimp.png"
+)
